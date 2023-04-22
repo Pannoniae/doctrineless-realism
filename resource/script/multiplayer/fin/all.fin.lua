dfe694f76a82d16232cd46a34adf70b5f9611afb
Pannoniae <prisznyak.zsombor@gmail.com>
2023-04-22 19:34:39 +0100

    start of all units mode

---
+++ resource/script/multiplayer/fin/all.fin.lua
@@ -0,0 +1,156 @@
+Purchases["late.fin"] = {
+	{Repeat = 0,  --infinite
+		Units = {
+			---[====[
+			-- Infantry Squads
+				---[[
+				{priority = 1.0, type = {"Infantry", "Squad", "Class3",}, unit = "squad_reserves_late(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class2",}, unit = "squad_scout_late(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class2",}, unit = "squad_rifle_late(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "AT", "Class2",}, unit = "squad_antitank_late(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class1",}, unit = "squad_assault(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class1",}, unit = "squad_cav_late(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class2",}, unit = "squad_jag_late(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "AT", "Class3",}, unit = "squad_jag_antitank_late(fin)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class1",}, unit = "squad_border_jag_late(fin)"},
+				--]]
+
+			-- Infantry Singles / Teams
+				---[[
+				{priority = 1.0, type = {"Infantry", "Team", "Aux", "Class3",}, unit = "single_auxiliary(fin)"},
+				{priority = 1.0, type = {"Infantry", "Team", "Class2",}, unit = "single_mg(fin)"},
+				{priority = 1.0, type = {"Infantry", "Team", "AT", "Class1",}, unit = "single_at(fin)"},
+				{priority = 1.0, type = {"Infantry", "Team", "AT", "Class2",}, unit = "single_bazooker(fin)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_at_miner(fin)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_engineer(fin)"},
+				{priority = 1.0, type = {"Infantry", "Team", "Class3",}, unit = "single_flamer(fin)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_tankman(fin)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_medic(fin)"},
+				{priority = 1.0, type = {"Infantry", "Team", "Class2",}, unit = "single_sniper(fin)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_officer(fin)"},
+				--]]
+
+			-- Cannons
+				---[[
+				--{priority = 1.0, type = {"Cannon", "Supply", "Ammo",}, unit = "inf_crate_fin"},
+				
+				{priority = 1.0, type = {"Cannon", "MG", "Class2",}, unit = "maxim_m1910_30"},
+				{priority = 1.0, type = {"Cannon", "MG", "Class2",}, unit = "ds39_stand_fin"},
+				{priority = 1.0, type = {"Cannon", "AA", "Class2",}, unit = "20mm_itk35"},
+				{priority = 1.0, type = {"Cannon", "AA", "Class2",}, unit = "20mm_itk40"},
+
+				{priority = 1.0, type = {"Cannon", "AT", "Class2",}, unit = "20mm_l39"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class3",}, unit = "37mm_pstk36"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class3",}, unit = "45mm_pstk32"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class3",}, unit = "47mm_pstk39"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class3",}, unit = "76mm_k02"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class2",}, unit = "75mm_pstk9738"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class2",}, unit = "76mm_k36"},
+				{priority = 1.0, type = {"Cannon", "AA", "AT", "Class1",}, unit = "75mm_itk37"},
+
+				{priority = 1.0, type = {"Cannon", "Mortar", "Class1",}, unit = "50mm_krh38"},
+				{priority = 1.0, type = {"Cannon", "Mortar", "Class1",}, unit = "81mm_krh36"},
+
+				{priority = 1.0, type = {"Cannon", "Support", "Class1",}, unit = "76mm_m1927_fin"},
+				{priority = 1.0, type = {"Cannon", "Artillery", "Class3",}, unit = "105mm_k13"},
+				--]]
+
+			-- Wheeled vehicles
+				---[[
+				{priority = 1.0, type = {"Vehicle", "AA", "Class1",}, unit = "ford_3ton_breda"},
+				{priority = 1.0, type = {"Armored", "MG", "Class2",}, unit = "l182"},
+				{priority = 1.0, type = {"Armored", "Class3",}, unit = "ba10_fin"},
+
+				--{priority = 1.0, type = {"Vehicle", "Unarmed", "Transport",}, unit = "m42_truppenfahrrad"},
+				--{priority = 1.0, type = {"Vehicle", "Unarmed", "Transport",}, unit = "ford_v3000"},
+				--{priority = 1.0, type = {"Vehicle", "Unarmed", "Supply", "Ammo",}, unit = "ford_v3000_ammo"},
+				--{priority = 1.0, type = {"Vehicle", "Unarmed", "Supply", "Fuel",}, unit = "ford_v3000_fuel"},
+				--{priority = 1.0, type = {"Vehicle", "Unarmed","Supply", "Engineer",}, unit = "ford_v3000_eng"},
+				--]]
+
+			-- Tankettes
+				---[[
+				{priority = 1.0, type = {"Armored", "MG", "Class3",}, unit = "t20_fin"},
+				--]]
+
+			-- Tanks
+				---[[
+				--{priority = 1.0, type = {"Tank", "Light", "Class3",}, unit = "ft17_mg_fin"},
+				--{priority = 1.0, type = {"Tank", "Light", "Class3",}, unit = "ft17_fin"},
+				{priority = 1.0, type = {"Tank", "Light", "Class3",}, unit = "fin_captured_v_t26_late"},
+				{priority = 1.0, type = {"Tank", "Light", "Class3",}, unit = "vickers_6t_altb"},
+				{priority = 1.0, type = {"Tank", "Light", "Class3",}, unit = "t26e"},
+				{priority = 1.0, type = {"Tank", "Light", "Class3",}, unit = "kht130_fin"},
+				{priority = 1.0, type = {"Tank", "Light", "Class2",}, unit = "bt42"},
+
+				{priority = 1.0, type = {"Tank", "Medium", "Class3",}, unit = "t28_38_fin"},
+				{priority = 1.0, type = {"Tank", "Medium", "Class2",}, unit = "fin_captured_v_t3476"},
+				{priority = 1.0, type = {"Tank", "Medium", "Class1",}, unit = "fin_captured_v_t3485"},
+				--]]
+
+			--- Self-Propelled Guns
+				---[[
+				{priority = 1.0, type = {"Tank", "Medium", "AT", "Class2",}, unit = "stug3g_fin"},
+				--]]
+
+			-- Doctrine All Around = "Wehrmacht Supplies II"
+				---[[
+				{priority = 1.0, type = { "Cannon", "MG", "Class1",}, unit = "panzernest_krab"},
+				--{priority = 1.0, type = { "Cannon", "AT", "Class1", "Class1",}, unit = "doctrine_75mm_pak40_fin_late"}, --towed
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class1",}, unit = "100mm_nbw35"},
+				
+				--{priority = 1.0, type = { "Class2", "Class4",}, unit = "doctrine_squad_signaller_late(fin)"},
+				--{priority = 1.0, type = { "Cannon", "Artillery", "Class2", "Class2",}, unit = "doctrine_105mm_h33"}, --towed
+				
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "150mm_h40"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "210mm_nebelwerfer42"},
+				{priority = 1.0, type = { "Tank", "Light", "Artillery", "Class3",}, unit = "pz38h_w40"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "150mm_rkh41"},
+				--]]
+
+			-- Doctrine Defensive = "Karelian Wall"
+				---[[
+				{priority = 1.0, type = { "Class1",}, unit = "doctrine_squad_engineer_late(fin)"},
+				{priority = 1.0, type = { "Class1",}, unit = "doctrine_heavy_engineer_late(fin)"},
+				{priority = 1.0, type = { "Cannon", "AT", "Class1",}, unit = "76mm_k02_30_40"},
+
+				{priority = 1.0, type = { "Tank", "Heavy", "Class2",}, unit = "kv1_40e_fin"},
+				{priority = 1.0, type = { "Cannon", "Mortar", "Class2",}, unit = "120mm_krh40"},
+				{priority = 1.0, type = { "Cannon", "AT", "Class2",}, unit = "75mm_k44"},
+
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "122mm_m1910_fin"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "203mm_h17"},
+				--]]
+
+			-- Doctrine Irregular = "Precision Strikes"
+				---[[
+				{priority = 1.0, type = { "Class1",}, unit = "doctrine_white_death_late"},
+				{priority = 1.0, type = { "Cannon", "Spotlight", "Class1",}, unit = "150mm_sw34"},
+				{priority = 1.0, type = { "Cannon", "AA", "Class1",}, unit = "40mm_itk38b"},
+
+				{priority = 1.0, type = { "Class2",}, unit = "doctrine_squad_sissi_late(fin)"},
+				{priority = 1.0, type = { "Tank", "Heavy", "Class2",}, unit = "kv1_42_fin"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class2",}, unit = "107mm_k10"},
+
+				--{priority = 1.0, type = { "Plane", "Airstrike", "Class3",}, unit = "doctrine_blenheim_late"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "105mm_k34"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "155mm_h17"},
+				--]]
+
+			-- Doctrine Offensive = "Aggressive Push"
+				---[[
+				{priority = 1.0, type = { "Class1",}, unit = "doctrine_squad_marksmen_late(fin)"},
+				{priority = 1.0, type = { "Tank", "Light", "Class1",}, unit = "t50_fin"}, 
+				{priority = 1.0, type = { "Tank", "Light", "AA", "Class1",}, unit = "l62"},
+
+				{priority = 1.0, type = { "Cannon", "Support", "Class2",}, unit = "84mm_k18_late"},
+				{priority = 1.0, type = { "Tank", "Medium", "Class2",}, unit = "panzer4j_fin"},
+				{priority = 1.0, type = { "Cannon", "Mortar", "Class2",}, unit = "170mm_minewerfer"},
+
+				{priority = 1.0, type = { "Tank", "Heavy", "Class3",}, unit = "isu152_fin"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "120mm_k78_31"},
+				--]]
+			--]====]
+		}
+	}
+}
---
+++ resource/script/multiplayer/rus/all.rus.lua
@@ -0,0 +1,169 @@
+Purchases["late.rus"] = {
+	{Repeat = 0,  --infinite
+		Units = {
+			---[====[
+			-- Infantry Squads
+				---[[
+				{priority = 1.0, type = {"Infantry", "Squad", "Class3",}, unit = "squad_conscripts_late(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class3",}, unit = "squad_partisan(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class2",}, unit = "squad_recon_late(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class2",}, unit = "squad_rifleunit(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class1",}, unit = "squad_smg(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "AT", "Class3",}, unit = "squad_at_rifle(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class1",}, unit = "squad_guardsrifle(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class1",}, unit = "squad_guards_smg_heavy(rus)"},
+				{priority = 1.0, type = {"Infantry", "Squad", "Class1",}, unit = "squad_marines_late(rus)"},
+				--]]
+
+			-- Infantry Singles / Teams
+				---[[
+				{priority = 1.0, type = {"Infantry", "Team", "Aux", "Class3",}, unit = "single_auxiliary(rus)"},
+				{priority = 1.0, type = {"Infantry", "Team", "Class2",}, unit = "single_mg(rus)"},
+				{priority = 1.0, type = {"Infantry", "Team", "AT", "Class1",}, unit = "single_at(rus)"},
+				{priority = 1.0, type = {"Infantry", "Team", "AT", "Class2",}, unit = "single_bazooker(rus)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_ap_miner(rus)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_at_miner(rus)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_engineer(rus)"},
+				{priority = 1.0, type = {"Infantry", "Team", "Class3",}, unit = "single_flamer(rus)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_tankman(rus)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_medic(rus)"},
+				{priority = 1.0, type = {"Infantry", "Team", "Class2",}, unit = "single_sniper(rus)"},
+				--{priority = 1.0, type = {"Infantry", "Team",}, unit = "single_officer(rus)"},
+				--]]
+
+			-- Cannons
+				---[[
+				--{priority = 1.0, type = {"Cannon", "Supply", "Ammo",}, unit = "inf_crate_rus"},
+
+				{priority = 1.0, type = {"Cannon", "MG", "Class2",}, unit = "sg43_stand"},
+				{priority = 1.0, type = {"Cannon", "MG", "Class2",}, unit = "dshk_stan"},
+				{priority = 1.0, type = {"Cannon", "AA", "Class2",}, unit = "25mm_72k"},
+
+				{priority = 1.0, type = {"Cannon", "AT", "Class3",}, unit = "45mm_m42"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class1",}, unit = "57mm_zis2_late"},
+				{priority = 1.0, type = {"Cannon", "AT", "Class2",}, unit = "76mm_zis3"},
+
+				{priority = 1.0, type = {"Cannon", "Support", "Class1",}, unit = "76mm_m1927"},
+				{priority = 1.0, type = {"Cannon", "Mortar", "Class1",}, unit = "82mm_bm37"},
+				{priority = 1.0, type = {"Cannon", "Artillery", "Class3",}, unit = "122mm_m30"},
+				--]]
+
+			-- Wheeled vehicles
+				---[[
+				{priority = 1.0, type = {"Vehicle", "MG", "Class3",}, unit = "m72"},
+				{priority = 1.0, type = {"Vehicle", "MG", "Class3",}, unit = "gaz67b"},
+				{priority = 1.0, type = {"Vehicle", "AA", "Class2",}, unit = "zis5_dshk"},
+				{priority = 1.0, type = {"Vehicle", "AA", "Class1",}, unit = "gaz_aaa_72k"},
+
+				{priority = 1.0, type = {"Armored", "MG", "Class2",}, unit = "ba64"},
+				{priority = 1.0, type = {"Armored", "Class3",}, unit = "ba10"},
+
+				--{priority = 1.0, type = {"Vehicle", "Unarmed", "Transport",}, unit = "zis5"},
+				--{priority = 1.0, type = {"Vehicle", "Unarmed", "Supply", "Ammo",}, unit = "gaz_aaa_supply"},
+				--{priority = 1.0, type = {"Vehicle", "Unarmed", "Supply", "Fuel",}, unit = "gaz_fuel"},
+				--{priority = 1.0, type = {"Vehicle", "Unarmed","Supply", "Engineer",}, unit = "zis5eng"},
+				--]]
+
+			-- Tankettes
+				---[[
+				{priority = 1.0, type = {"Armored", "MG", "Class3",}, unit = "t20"},
+				--]]
+
+			-- Tanks
+				---[[
+				{priority = 1.0, type = {"Tank", "Light", "Class3",}, unit = "t70"},
+
+				{priority = 1.0, type = {"Tank", "Medium", "Class2",}, unit = "t3476_43"},
+				{priority = 1.0, type = {"Tank", "Medium", "Class1",}, unit = "t3485_44"},
+
+				{priority = 1.0, type = {"Tank", "Heavy", "Class3",}, unit = "kv1s"},
+				{priority = 1.0, type = {"Tank", "Heavy", "Class2",}, unit = "is1_43"},
+				{priority = 1.0, type = {"Tank", "Heavy", "Class3",}, unit = "is2_43"},
+				--]]
+
+			-- Self-Propelled Guns
+				---[[
+				{priority = 1.0, type = {"Tank", "Light", "Support", "Class3",}, unit = "su76"},
+				{priority = 1.0, type = {"Tank", "Medium", "AT", "Class2",}, unit = "su85"},
+				{priority = 1.0, type = {"Tank", "Medium", "Support", "Class3",}, unit = "su122"},
+				{priority = 1.0, type = {"Tank", "Heavy", "AT", "Class2",}, unit = "su152"},
+				--]]
+
+			-- Doctrine All Around = "Lend-lease"
+				---[[
+				{priority = 1.0, type = { "Armored", "MG", "Class1",}, unit = "mmg_carrier_rus"},
+				{priority = 1.0, type = { "Tank", "Light", "Class1",}, unit = "m3_stuart_late_rus"},
+				{priority = 1.0, type = { "Tank", "Light", "Class1",}, unit = "valentine7"},
+
+				--{priority = 1.0, type = { "Class2",}, unit = "doctrine_squad_scoutcar_marines"},
+				{priority = 1.0, type = { "Tank", "Medium", "Class2",}, unit = "m4a2_75_late_rus"},
+				{priority = 1.0, type = { "Tank", "Heavy", "Class2",}, unit = "churchill3"},
+
+				--{priority = 1.0, type = { "Cannon", "AT", "Class3", "Class3",}, unit = "doctrine_100mm_bs3_scoutcar"}, --towed
+				{priority = 1.0, type = { "Tank", "Medium", "Class3",}, unit = "m4a2_76w_early_rus"},
+				--{priority = 1.0, type = {"Vehicle", "Artillery", "Class3", "Class4",}, unit = "doctrine_bm31"},
+				--]]
+
+			-- Doctrine Defensive = "Breakthrough"
+				---[[
+				--{priority = 1.0, type = { "Class1",}, unit = "doctrine_squad_sapperteam(rus)"},
+				--{priority = 1.0, type = { "Class1",}, unit = "doctrine_squad_rifle_motorised"},
+				{priority = 1.0, type = { "Cannon", "Support", "Class1",}, unit = "76mm_m43"},
+
+				--{priority = 1.0, type = { "Class2",}, unit = "doctrine_squad_heavymarines(rus)"},
+				{priority = 1.0, type = { "Cannon", "Mortar", "Class2",}, unit = "120mm_pm38"},
+				{priority = 1.0, type = { "Cannon", "AA", "AT", "Class2",}, unit = "85mm_52k"},
+
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "152mm_ml20"},
+				{priority = 1.0, type = { "Tank", "Medium", "AT", "Class3",}, unit = "su100"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "203mm_b4"},
+				--]]
+
+			-- Doctrine Irregular = "Demolition"
+				---[[
+				{priority = 1.0, type = { "Cannon", "MG", "Class1",}, unit = "dshk_aa"},
+				--{priority = 1.0, type = { "Class1",}, unit = "doctrine_squad_spetsnaz_late(rus)"}, --TODO: Causes Crash, unknown.
+				{priority = 1.0, type = { "Cannon", "AA", "Class1",}, unit = "37mm_61k"},
+
+				{priority = 1.0, type = { "Class2",}, unit = "doctrine_squad_heavyrecon(rus)"},
+				{priority = 1.0, type = { "Tank", "Medium", "Class2",}, unit = "t3476_43_flame"},
+				{priority = 1.0, type = { "Tank", "Medium", "Class2",}, unit = "t3457_43"},
+
+				--{priority = 1.0, type = { "Plane", "Airstrike", "Class3",}, unit = "doctrine_il2_late"},
+				{priority = 1.0, type = { "Tank", "Heavy", "AT", "Class3",}, unit = "isu122s"},
+				{priority = 1.0, type = { "Vehicle", "Artillery", "Class3",}, unit = "bm13_studebaker"},
+				--]]
+
+			-- Doctrine Offensive = "Shock Army"
+				---[[
+				--{priority = 1.0, type = { "Class1",}, unit = "doctrine_squad_faustniki(rus)"},
+				{priority = 1.0, type = { "Tank", "Light", "Class1",}, unit = "t60"},
+				{priority = 1.0, type = { "Tank", "Light", "Class1",}, unit = "t70m"},
+
+				--{priority = 1.0, type = { "Class2",}, unit = "doctrine_squad_flame(rus)"},
+				--{priority = 1.0, type = { "Class2",}, unit = "doctrine_squad_rus_signaller_late(rus)"},
+				{priority = 1.0, type = { "Tank", "Heavy", "Class2",}, unit = "kv85"},
+
+				{priority = 1.0, type = { "Tank", "Medium", "Class3",}, unit = "t3485_44"},
+				{priority = 1.0, type = { "Tank", "Heavy", "AT", "Class3",}, unit = "isu152"},
+				{priority = 1.0, type = { "Tank", "Heavy", "Class3",}, unit = "is2_44"},
+				--]]
+
+			-- Doctrine Support = "Iron Curtain"
+				---[[
+				--{priority = 1.0, type = { "Infantry", "Team", "Class1",}, unit = "doctrine_squad_rus_politicalofficer_late(rus)"},
+				--{priority = 1.0, type = { "Vehicle", "MG", "Infantry", "Team", "Class1",}, unit = "doctrine_m72_snipers_late"},
+				{priority = 1.0, type = { "Tank", "Light", "Class1",}, unit = "t50"},
+
+				--{priority = 1.0, type = { "Plane", "Recon", "Class2",}, unit = "doctrine_po2_late"},
+				{priority = 1.0, type = { "Tank", "Medium", "Class2",}, unit = "t3485_44b"},
+				{priority = 1.0, type = { "Tank", "Medium", "AT", "Class2",}, unit = "su85m"},
+
+				{priority = 1.0, type = { "Tank", "Medium", "Class3",}, unit = "t3485_44a_flame"},
+				{priority = 1.0, type = { "Tank", "Heavy", "Class3",}, unit = "is2_45"},
+				{priority = 1.0, type = { "Cannon", "Artillery", "Class3",}, unit = "280mm_br5"},
+				--]]
+			--]====]
+		}
+	}
+}
